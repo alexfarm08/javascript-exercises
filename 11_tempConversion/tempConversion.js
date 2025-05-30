1dde7ba92c0d0a55cf0cc1b7d3e299d90aefe7fd
alexfarm08 <209876587+alexfarm08@users.noreply.github.com>
2025-05-30 11:52:10 -0700

    all test passed may 30, 2025

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) / 1.8;
+  let rounded = Math.round(celsius * 10) / 10;
+  return rounded;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (1.8 * celsius) + 32;
+  let rounded = Math.round(fahrenheit * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
