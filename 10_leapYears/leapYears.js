9b7cef8dd18ed5d5ae54946991fd44896e4304ff
alexfarm08 <209876587+alexfarm08@users.noreply.github.com>
2025-05-30 11:19:00 -0700

    all test passed may 30, 2025

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let condition1 = year % 4;
+    let condition2 = year % 100;
+    let condition3 = year % 400;
+
+    if (condition1 == 0) {
+        if (condition2 == 0) {
+            if (condition3 == 0) {
+                return true;
+            }
+            return false;
+        }
+        else if (!condition2 == 0) {
+            return true;
+        }
+    }
+    else {
+        return false;
+    }
 
 };
 
