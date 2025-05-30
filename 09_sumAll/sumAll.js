f8c6ede3fe7c9f6ca46831e622030abc9f2a3775
alexfarm08 <209876587+alexfarm08@users.noreply.github.com>
2025-05-30 10:53:58 -0700

    all tests passed may 30, 2025

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+
+    if (start <= 0 || end <= 0) {
+        return "ERROR";
+    }
+    else if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
+
+    if (start < end) {
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    else if (end < start) {
+        for (let j = start; j >= end; j--) {
+            sum += j;
+        }
+        return sum;
+    }
 
 };
 
