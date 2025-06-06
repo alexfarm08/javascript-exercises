5fd3eec4dfc266fda67eace929330782da74b6f1
alexfarm08 <209876587+alexfarm08@users.noreply.github.com>
2025-06-06 15:16:10 -0700

    all tests passed jun 6, 2025

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	let add = num1 + num2;
+  return add;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	let sub = num1 - num2;
+  return sub;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let mul = 1;
 
+  for (let i = 0; i < array.length; i++) {
+    mul *= array[i];
+  }
+
+  return mul;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let pow = Math.pow(num1,num2);
+  return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fac = 1;
+
+  for (let i = 1; i <= num; i++) {
+    fac *= i;
+  }
+  return Number(fac);
 };
 
 // Do not edit below this line
