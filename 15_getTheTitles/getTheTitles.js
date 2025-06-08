6cf32d35b787e4709bbc029db9942099487c5426
alexfarm08 <209876587+alexfarm08@users.noreply.github.com>
2025-06-08 10:57:49 -0700

    all tests passed jun 8, 2025

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let newArr = array.reduce((arr,book) => {
+        arr.push(book.title)
+        return(arr)
+    }, []);
+    return newArr;
 };
 
 // Do not edit below this line
